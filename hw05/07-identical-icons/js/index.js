452f91e704b823f0645de583059e64eeb330e330
Lorenzo Siy <lorenzosiy2019@u.northwestern.edu>
2019-05-06 17:10:03 -0500

    Adding hw05 to the repo

---
+++ hw05/07-identical-icons/js/index.js
@@ -0,0 +1,21 @@
+const elem = document.getElementById('output');
+
+// Todo: make
+const className = 'fa-heart';
+
+/* TODO: instead of repeating the line below
+   over and over again, use a while loop to repeat it
+   as many times as you like.
+*/
+
+var count = 0;
+
+while (count < 100){
+  elem.innerHTML += '<i class="fa ' + className + '"></i>';
+  count += 1; 
+}
+
+// elem.innerHTML += '<i class="fa ' + className + '"></i>';
+// elem.innerHTML += '<i class="fa ' + className + '"></i>';
+// elem.innerHTML += '<i class="fa ' + className + '"></i>';
+// elem.innerHTML += '<i class="fa ' + className + '"></i>';
