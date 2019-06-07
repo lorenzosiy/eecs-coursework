19af72f0437e1a422a89f63bf83721ef2908a9bd
Lorenzo Siy <lorenzosiy2019@u.northwestern.edu>
2019-06-06 23:40:54 -0500

    Adding current state of my project to the repo

--- project/js/index.js
+++ project/js/index.js
@@ -2,6 +2,8 @@ const initEventHandlers = () => {
   for (elem of document.querySelectorAll('.nav a')) {
     elem.onclick = scrollToAnchor;
   }
+
+  document.querySelector('#backToTop').onclick = scrollToTop;
 }
 
 const scrollToAnchor = (ev) => {
@@ -9,10 +11,35 @@ const scrollToAnchor = (ev) => {
         return Math.floor(el.getBoundingClientRect().top);
       };
         var targetID = ev.target.getAttribute('href');
+        console.log(targetID);
         const targetAnchor = document.querySelector(targetID);
         window.scrollBy({ top: distanceToTop(targetAnchor), left: 0, behavior:
           'smooth'});
         ev.preventDefault();
     }
+
+const scrollToTop = (ev) => {
+    const distanceToTop = (el) => {
+        return Math.floor(el.getBoundingClientRect().top);
+      };
+        var targetID = ev.target.getAttribute('href');
+        console.log(targetID);
+        const targetAnchor = document.querySelector(targetID);
+        window.scrollBy({ top: distanceToTop(targetAnchor), left: 0, behavior:
+          'smooth'});
+        console.log(distanceToTop(targetAnchor));
+        ev.preventDefault();
+    }
+
+
+// $(document).ready(function() {
+//     $("#backToTop").click(function(event) {
+//         event.preventDefault();
+//         $("html, body").animate({ scrollTop: 0 }, "smooth");
+//         return false;
+//     });
+//
+// });
+
 
 initEventHandlers();
