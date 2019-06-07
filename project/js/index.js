bfa6a16ac009b0447447bf4df487df32d5abc1c1
Lorenzo Siy <lorenzosiy2019@u.northwestern.edu>
2019-06-07 15:46:58 -0500

    Adding current state of my project onto the repo

--- project/js/index.js
+++ project/js/index.js
@@ -22,24 +22,13 @@ const scrollToTop = (ev) => {
     const distanceToTop = (el) => {
         return Math.floor(el.getBoundingClientRect().top);
       };
-        var targetID = ev.target.getAttribute('href');
-        console.log(targetID);
+        var targetID = ev.target.getAttribute('target');
         const targetAnchor = document.querySelector(targetID);
         window.scrollBy({ top: distanceToTop(targetAnchor), left: 0, behavior:
           'smooth'});
         console.log(distanceToTop(targetAnchor));
         ev.preventDefault();
     }
-
-
-// $(document).ready(function() {
-//     $("#backToTop").click(function(event) {
-//         event.preventDefault();
-//         $("html, body").animate({ scrollTop: 0 }, "smooth");
-//         return false;
-//     });
-//
-// });
 
 
 initEventHandlers();
